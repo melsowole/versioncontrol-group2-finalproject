c8720c973cff563950527dacf3d8d40a6fb8913e
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-15 20:08:05 +0100

    added basic file structure dom.js

---
+++ modules/dom.js
@@ -0,0 +1,40 @@
+export const dom = {
+	create,
+	createAndAppend,
+};
+
+function create(tagName, classes, textContent) {
+	if (!tagName) {
+		throw "ERROR: The 'tagName' parameter in dom.create() is missing";
+	}
+
+	const element = document.createElement(tagName);
+
+	if (classes) {
+		if (typeof classes === "string") {
+			classes = [classes];
+		}
+
+		classes.forEach((cls) => element.classList.add(cls));
+	}
+
+	if (textContent) {
+		element.textContent = textContent;
+	}
+
+	return element;
+}
+
+function createAndAppend(parent, tagName, classes, textContent) {
+	if (!(parent instanceof HTMLElement)) {
+		throw "ERROR: The 'parent' argument in dom.createAndAppend() must be a valid DOM element.";
+	}
+
+	if (!tagName) {
+		throw "ERROR: The 'tagName' argument in dom.createAndAppend() must be a valid HTML tag name.";
+	}
+
+	const element = create(tagName, classes, textContent);
+
+	parent.append(element);
+}
