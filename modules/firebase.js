f6f4ac04e7d3db7f48a8751b5b4659547224739b
melsowole <82158719+melsowole@users.noreply.github.com>
2024-01-16 11:29:11 +0100

    added firebase functions

--- modules/firebase.js
+++ modules/firebase.js
@@ -5,14 +5,62 @@ export const firebase = {
 const URL =
 	"https://fe23-messageboard-default-rtdb.europe-west1.firebasedatabase.app/.json";
 
-function GET() {
-	// fetches all data
-	// returns data
+async function GET() {
+	const response = await fetch(URL);
+
+	const data = await response.json();
+
+	return data;
 }
 
-function POST(message) {
-	// Adds message to database
-	// returns id of added obj, or false
-	// throws Error if message is not provided
-	// throws Error if all keys are not provided
+async function POST(obj) {
+	const OPTIONS = {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(obj),
+	};
+
+	const response = await fetch(URL, OPTIONS);
+
+	if (response.ok) {
+		const data = await response.json();
+
+		return data.name;
+	}
+
+	return false;
+}
+
+async function DELETE(id) {
+	const data = await GET();
+
+	if (data[id]) {
+		delete data[id];
+
+		await PUT(data);
+
+		GET();
+
+		return data;
+	} else {
+		return false;
+	}
+}
+
+async function PUT(newObj) {
+	const OPTIONS = {
+		method: "PUT",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify(newObj),
+	};
+
+	const response = await fetch(URL, OPTIONS);
+
+	const data = await response.json();
+
+	if (response.ok) {
+		return true;
+	}
+
+	return false;
 }
