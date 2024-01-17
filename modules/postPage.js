a4e706453fd41f32f4fa414c9ae82a241bac6063
Amandaanderssn <tindraamanda@hotmail.se>
2024-01-17 22:18:37 +0100

    commitar för att kunna pulla

--- modules/postPage.js
+++ modules/postPage.js
@@ -1,7 +1,24 @@
 import { dom } from "./dom.js";
+import { firebase } from "./firebase.js";
 
-function renderPostPage() {
-  dom.create("section", ".PostPageSection");
+// function renderPostPage() {
+//   dom.create("section", ".PostPageSection");
+// }
+
+document.body.append(createPostHeader());
+
+function createPostHeader() {
+  const chatHeader = dom.create("header", "chatHeading", "Chat");
+  const newPostButton = dom.createAndAppend(
+    chatHeader,
+    "button",
+    "newPost",
+    "Add new post"
+  );
+
+  return chatHeader;
 }
 
-renderPostPage();
+// newPostButton.addEventlistener("click", (event) => {
+//   event.preventDefault();
+// });
