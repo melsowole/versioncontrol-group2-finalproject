38e0ba5f0c8ca3f7c3ec63ab6218f8bdf80aa30a
Elvira Ericsson <elvira.rigi@hotmail.com>
2024-01-17 19:13:22 +0100

    created about-page

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import { dom } from "./modules/dom.js";
 import { firebase } from "./modules/firebase.js";
 import { contactPage } from "./modules/contact.js";
+import { aboutPage } from "./modules/about.js";
 import { displayLanding } from "./modules/landing.js";
 
 displayLanding.then(() => {
   document.querySelector(".page-content").append(contactPage());
+  document.querySelector(".page-content").append(aboutPage());
 });
 
 
