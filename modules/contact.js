d6c3f6024e776c6d3ed241da0e8098f285ae2fdf
Yasir-coding <150331916+Yasir-coding@users.noreply.github.com>
2024-01-24 11:36:06 +0100

    contact form med alert och css

--- modules/contact.js
+++ modules/contact.js
@@ -33,5 +33,40 @@ export function contactPage() {
     "Mobile: 070-1234567"
   );
 
+  const form = dom.createAndAppend(
+    contactDivEl,
+    "form",
+    "contactForm",
+    ""
+
+  );
+
+  const contactFormH1 = dom.createAndAppend(
+    form,
+    "h3",
+    "contactFormH3",
+    "Send us a message"
+  );
+
+  const textArea = dom.createAndAppend(
+    form,
+    "textArea",
+    "contactTextArea",
+    ""
+  );
+
+  const button = dom.createAndAppend(
+    form,
+    "button",
+    "contactButton",
+    "send"
+  );
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    alert("Your message has been sent");
+    form.reset();
+  });
+
   return contactWrapper;
 }
